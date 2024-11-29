81a2c5895536e7c2c3b42718aadc0cce6c1fdb8d
Silvija Gudelytė <silvijagudelyte@gmail.com>
2024-11-29 10:21:24 +0200

    add css and js

--- script.js
+++ script.js
@@ -3,13 +3,20 @@ fetch('https://api.escuelajs.co/api/v1/users')
     .then(users => {
         const userContainer = document.getElementById('user-container');
         users.forEach(user => {
+            // Create a user card
             const userCard = document.createElement('div');
             userCard.classList.add('user-card');
             userCard.innerHTML = `
-        <img src="${user.avatar}" alt="">
+        <img src="${user.avatar}" alt="${user.name}">
         <h3>${user.name}</h3>
         <p>${user.email}</p>
+        <button class="delete-btn">Delete</button>
       `;
+
             userContainer.appendChild(userCard);
+            const deleteBtn = userCard.querySelector('.delete-btn');
+            deleteBtn.addEventListener('click', () => {
+                userCard.remove();
+            });
         });
     })
